84ae730d2e0d9c9ef57b58e38c398c55beb5a93b
Eric Swierczek <eric.swierczek@jamf.com>
2025-12-28 00:04:27 -0600

    Move unicafe back

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,51 +6,59 @@ const Button = ({onClick, text}) => {
   )
 }
 
+const StatisticLine = ({text, value, postfix}) => {
+  return (
+    <>
+      <tr>
+        <td>{text}</td>
+        <td>{value} {postfix}</td>
+      </tr>
+    </>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad;
+  const average = ((good - bad) / all).toPrecision(1);
+  const positive = ((good / all)).toPrecision(3) * 100;
+
+  if (all > 0) {
+    return(
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} postfix='%' />
+        </tbody>
+      </table>
+    )
+  } else {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+}
+
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
-  const [selected, setSelected] = useState(0)
-  const [maxVote, setMaxVote] = useState(0);
-
-  const countVote = selected => {
-    const copy = [...votes]
-    copy[selected] += 1
-    setVotes(copy)
-
-    const maxVoteIndex = copy.indexOf(Math.max(...copy));
-    setMaxVote(maxVoteIndex);
-  }
-
-  const randomAnecdote = () => {
-    const randomIndex = Math.ceil(Math.random() * anecdotes.length) - 1;
-    setSelected(randomIndex);
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <p>
-        {anecdotes[selected]}<br />
-        has {votes[selected]} votes
-      </p>
-      <Button onClick={() => countVote(selected)} text='vote' />
-      <Button onClick={() => randomAnecdote()} text='next anecdote' />
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good+1)} text='good' />
+      <Button onClick={() => setNeutral(neutral+1)} text='neutral' />
+      <Button onClick={() => setBad(bad+1)} text='bad' />
 
-      <h1>Anecdote with the most votes</h1>
-      <p>
-        {anecdotes[maxVote]}<br />
-        has {votes[maxVote]} votes
-      </p>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
