7439c210754a814a6b3f0fb0f5b0c8f56942588f
Eric Swierczek <eric.swierczek@jamf.com>
2025-12-28 22:43:46 -0600

    Ex 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,53 @@
+const Course = ({course}) => {
+  return (
+    <>
+      <Header text={course.name} />
+
+      {course.parts.map(part => {
+        return (
+          <Content key={part.id} text={part.name} exercises={part.exercises} />
+        )
+      })}
+    </>
+  )
+}
+
+const Header = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Content = ({id, text, exercises}) => {
+  return (
+    <p>{text} {exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
