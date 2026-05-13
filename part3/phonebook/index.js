de52537f996ad421bd9827c5b6a42ae48fb9de91
Eric Swierczek <eric.swierczek@jamf.com>
2026-05-13 14:49:58 -0500

    Ex 3.3 and 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -34,6 +34,24 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${(new Date()).toLocaleString()}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
