8a4354b2f0ac0a543b52b0ebe2a0ad36538028e8
Eric Swierczek <eric.swierczek@jamf.com>
2026-05-13 15:19:52 -0500

    Ex 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,20 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+  // console.log(req.method);
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'),
+    '-',
+    tokens['response-time'](req, res),
+    'ms',
+    req.method == 'POST' ? JSON.stringify(req.body) : ''
+  ].join(' ')
+}))
 
 let persons = [
     {
