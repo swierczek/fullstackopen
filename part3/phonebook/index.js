f39ed067f270321a9641629ffd9e59ac924040f9
Eric Swierczek <eric.swierczek@jamf.com>
2026-05-13 14:54:48 -0500

    Ex 3.5 and 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -52,6 +52,32 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Name or number missing'
+    })
+  }
+
+  if (persons.filter(p => p.name == body.name).length > 0) {
+    return response.status(400).json({
+      error: 'Name already exists'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random()*1000000),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
