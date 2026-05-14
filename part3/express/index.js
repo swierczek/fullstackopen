bf338e80f47dbbf94893f9be25c026988a43cbf5
Eric Swierczek <eric.swierczek@jamf.com>
2026-05-14 14:00:47 -0500

    uibuild

--- part3/express/index.js
+++ part3/express/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 
 let notes = [
@@ -41,7 +40,6 @@ const generateId = () => {
 
 app.use(express.json())
 app.use(requestLogger)
-app.use(cors())
 app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
